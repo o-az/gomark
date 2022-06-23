76ca770cadc97b51f400ac3ac4d0f5e22bcab295
o-az <23618431+o-az@users.noreply.github.com>
2022-06-22 20:26:53 -0500

    handle GET

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/base64"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -9,23 +10,29 @@ import (
 
 func handleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	if len(request.Body) == 0 {
-		return events.APIGatewayProxyResponse{
-			StatusCode:        400,
-			Headers:           map[string]string{},
-			MultiValueHeaders: map[string][]string{},
-			Body:              "No body provided",
-			IsBase64Encoded:   false,
-		}, nil
+		return events.APIGatewayProxyResponse{StatusCode: 400, IsBase64Encoded: false}, nil
+	}
+	var compiledMarkdown string
+	switch request.HTTPMethod {
+	case "GET":
+		{
+			decoded, err := base64.StdEncoding.DecodeString(request.QueryStringParameters["base64"])
+			compiledMarkdown = string(decoded)
+			if err != nil {
+				return events.APIGatewayProxyResponse{StatusCode: 400, IsBase64Encoded: false}, nil
+			}
+		}
+	case "POST":
+		{
+			compiledMarkdown = compileMarkdown(request.Body)
+		}
+	default:
+		{
+			compiledMarkdown = "Invalid method"
+		}
 	}
 
-	var compiledMarkdown = compileMarkdown(request.Body)
-	return events.APIGatewayProxyResponse{
-		StatusCode:        200,
-		Headers:           map[string]string{},
-		MultiValueHeaders: map[string][]string{},
-		Body:              compiledMarkdown,
-		IsBase64Encoded:   false,
-	}, nil
+	return events.APIGatewayProxyResponse{StatusCode: 200, Body: compiledMarkdown}, nil
 }
 
 func main() {
