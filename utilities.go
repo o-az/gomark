c704178492a188be3bfac8a3ce9f728f348a2955
o-az <23618431+o-az@users.noreply.github.com>
2022-06-23 10:11:33 -0500

    gh action

--- utilities.go
+++ utilities.go
@@ -1,30 +1,32 @@
 package main
 
-import (
-	"fmt"
-	"io/ioutil"
-	"log"
-	"os"
-)
+// TODO: Add support for loading file
 
-func readFile(filepath string) string {
-	body, err := ioutil.ReadFile(filepath)
-	if err != nil {
-		log.Fatalf("unable to read file: %v", err)
-	}
-	return string(body)
-}
+// import (
+// 	"fmt"
+// 	"io/ioutil"
+// 	"log"
+// 	"os"
+// )
 
-func writeFile(filepath string, content string) {
-	file, err := os.Create(filepath)
-	if err != nil {
-		log.Fatalf("unable to wrote file: %v", err)
-	}
-	defer file.Close()
-	_, err2 := file.WriteString(content)
+// func readFile(filepath string) string {
+// 	body, err := ioutil.ReadFile(filepath)
+// 	if err != nil {
+// 		log.Fatalf("unable to read file: %v", err)
+// 	}
+// 	return string(body)
+// }
 
-	if err2 != nil {
-		log.Fatalf("unable to wrote file: %v", err)
-	}
-	fmt.Println("done")
-}
+// func writeFile(filepath string, content string) {
+// 	file, err := os.Create(filepath)
+// 	if err != nil {
+// 		log.Fatalf("unable to wrote file: %v", err)
+// 	}
+// 	defer file.Close()
+// 	_, err2 := file.WriteString(content)
+
+// 	if err2 != nil {
+// 		log.Fatalf("unable to wrote file: %v", err)
+// 	}
+// 	fmt.Println("done")
+// }
